d1850fa90dd02d5dc5ba637212e3f653885f5b57
kurihara.sk <nikoand01@gmail.com>
2023-02-17 17:44:37 +0900

    add: delete post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import ActionIcons from "@/components/timeline/actionIcons";
 import { Post } from "@/utils/types";
 import { apiLocalhost } from "@/utils/urls/server";
-import { Alert, Card, CardActionArea, Snackbar } from "@mui/material";
+import {
+  Alert,
+  Card,
+  Snackbar,
+  TextareaAutosize,
+  TextField,
+} from "@mui/material";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../components/timeline/timeline.module.scss";
 import { RootState } from "@/redux/store";
 import { updatePosts, updateSelectedPost } from "@/redux/reducers/posts";
+import ConfirmationModal from "@/components/common/confirmationModal";
 
 const TimeLine = () => {
   const dispatch = useDispatch();
@@ -34,15 +42,60 @@ const TimeLine = () => {
     if (selectedPost.post === undefined) return;
     dispatch(updateSelectedPost({ ...selectedPost, editing: true }));
   };
-  const handleClickDeleteIcon = () => {};
+
+  const { register, handleSubmit } = useForm();
+
+  type onSubmitProp = {
+    title: string;
+    content: string;
+  };
+  const onSubmit = async (data: any) => {
+    const { title, content } = data as onSubmitProp;
+    // API叩く
+    const { post } = await apiLocalhost
+      .patch(`/posts/${selectedPost.post?.id}`, {
+        posts: { title, content },
+      })
+      .then((res) => res.data);
+    const updatedPosts = posts?.map((tgtPost) => {
+      if (tgtPost.id === post.id) {
+        return post;
+      } else {
+        return tgtPost;
+      }
+    });
+    dispatch(updatePosts(updatedPosts));
+    dispatch(updateSelectedPost({ ...selectedPost, editing: false }));
+  };
+  const [open, setOpen] = useState(false);
+  const handleDeleteModalOpen = () => {
+    setOpen(true);
+  };
+  const handleDeleteModalClose = () => {
+    setOpen(false);
+  };
+  const deletePost = async () => {
+    const postId = selectedPost.post?.id;
+    await apiLocalhost.delete(`/posts/${postId}`);
+    const updatedPosts = posts?.filter((post) => post.id !== postId);
+    dispatch(updatePosts(updatedPosts));
+    dispatch(updateSelectedPost({ post: undefined, editing: false }));
+    handleDeleteModalClose();
+  };
   const actionIconsFuncs = {
     handleClickMessageIcon,
     handleClickEditIcon,
-    handleClickDeleteIcon,
+    handleClickDeleteIcon: handleDeleteModalOpen,
   };
 
   return (
     <div style={{ width: "90%", margin: "0 auto" }}>
+      <ConfirmationModal
+        handleClose={handleDeleteModalClose}
+        confirmationTxt="本当にこの投稿を削除しますか？"
+        execFunc={deletePost}
+        open={open}
+      />
       {posts && posts.length > 0 ? (
         posts.map((post, index) => {
           return (
@@ -61,46 +114,65 @@ const TimeLine = () => {
                     {...actionIconsFuncs}
                     createdByCurrentUser={post.user_id === currentUser?.id}
                   />
-                  {selectedPost.editing === true && "aaa"}
                 </>
               )}
-              <CardActionArea>
-                <div className={styles.pokemonImageWrapper}>
+              <div className={styles.pokemonImageWrapper}>
+                <Image
+                  className={styles.pokemonImage}
+                  src={post.pokemon_image}
+                  alt={post.pokemon_name}
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.postHeader}>
+                <h1>{post.pokemon_name}</h1>
+                <h2 className={styles.test}>{post.version_name}</h2>
+                <h4>{post.generation_name}</h4>
+              </div>
+
+              <Alert
+                className={styles.userContainer}
+                icon={false}
+                variant="outlined"
+                severity="info"
+              >
+                <div>
                   <Image
-                    className={styles.pokemonImage}
-                    src={post.pokemon_image}
-                    alt={post.pokemon_name}
+                    src={post.user_img}
+                    alt={post.user_name}
                     width={100}
                     height={100}
                   />
+                  <span>{post.user_name} </span>
                 </div>
-                <div className={styles.postHeader}>
-                  <h1>{post.pokemon_name}</h1>
-                  <h2 className={styles.test}>{post.version_name}</h2>
-                  <h4>{post.generation_name}</h4>
-                </div>
-
-                <Alert
-                  className={styles.userContainer}
-                  icon={false}
-                  variant="outlined"
-                  severity="info"
-                >
-                  <div>
-                    <Image
-                      src={post.user_img}
-                      alt={post.user_name}
-                      width={100}
-                      height={100}
-                    />
-                    <span>{post.user_name} </span>
+              </Alert>
+              <div className={styles.postContainer}>
+                {selectedPost.post?.id === post.id && selectedPost.editing ? (
+                  <div className={styles.postFormWrapper}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      <button>編集を完了する</button>
+                      <TextField
+                        id="filled-basic"
+                        label="タイトル"
+                        variant="filled"
+                        type="text"
+                        defaultValue={selectedPost.post?.title}
+                        {...register("title", { required: true })}
+                      />
+                      <TextareaAutosize
+                        defaultValue={selectedPost.post?.content}
+                        {...register("content", { required: true })}
+                      />
+                    </form>
                   </div>
-                </Alert>
-                <div className={styles.postContainer}>
-                  <h2>{post.title}</h2>
-                  <p>{post.content}</p>
-                </div>
-              </CardActionArea>
+                ) : (
+                  <>
+                    <h2>{post.title}</h2>
+                    <p>{post.content}</p>
+                  </>
+                )}
+              </div>
             </Card>
           );
         })
@@ -110,4 +182,5 @@ const TimeLine = () => {
     </div>
   );
 };
+
 export default TimeLine;
