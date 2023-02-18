c93ccb95d68eb8126e233f7d289da241a531c2e1
kurihara.sk <nikoand01@gmail.com>
2023-02-18 15:47:49 +0900

    test: config server url

--- src/utils/urls/server.ts
+++ src/utils/urls/server.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+const serverUrl = () => {
+  if (process.env.NODE_ENV === "production") {
+    return "http://13.230.250.192:3000/api/v1/";
+  } else {
+    return "http://localhost:3001/api/v1";
+  }
+};
 export const apiLocalhost = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: serverUrl(),
 });
