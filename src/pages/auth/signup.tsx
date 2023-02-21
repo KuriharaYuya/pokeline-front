995f29abe7f1fb5e86f019df4e3a4338c276f5ac
kurihara.sk <nikoand01@gmail.com>
2023-02-21 22:20:22 +0900

    fix: warn multiple register

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,7 +1,7 @@
 import { signUpWithGoogle } from "@/features/auth";
 import { loginSuccess } from "@/redux/reducers/auth";
 import { timelinePath } from "@/utils/urls/client";
-import { Button } from "@mui/material";
+import { Alert, Button, Snackbar } from "@mui/material";
 import Router from "next/router";
 import React from "react";
 import { useDispatch } from "react-redux";
@@ -10,12 +10,32 @@ import styles from "../../components/common/global.module.scss";
 const Auth = () => {
   const dispatch = useDispatch();
   const onClickHandler = async () => {
-    const user = await signUpWithGoogle();
-    dispatch(loginSuccess(user));
-    Router.push(timelinePath);
+    const res = await signUpWithGoogle();
+
+    if (res === 400) {
+      setOpen(true);
+    } else {
+      const user = res.user;
+      dispatch(loginSuccess(user));
+      Router.push(timelinePath);
+    }
+  };
+  // snackbar関連の関数
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
   };
   return (
     <div>
+      {open && (
+        <Alert
+          onClose={handleClose}
+          severity="error"
+          style={{ marginBottom: "1rem" }}
+        >
+          既に登録されているユーザーです
+        </Alert>
+      )}
       <Button
         variant="contained"
         onClick={onClickHandler}
